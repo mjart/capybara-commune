ed1c45c03c86d9f9d984de961990c590e93b629e
FireFlyForLife <maiko.s.99@gmail.com>
2020-03-28 13:37:47 +0100

    Added texture to capybara-pelt

--- src/main/java/net/capybara/CapybaraMain.java
+++ src/main/java/net/capybara/CapybaraMain.java
@@ -8,19 +8,21 @@ import net.minecraft.util.registry.Registry;
 
 
 public class CapybaraMain implements ModInitializer {
+	public static final String CAPYBARA_NAMESPACE = "capybara-commune";
 
 	public static final Item OAK_BARK = new Item(new Item.Settings().group(ItemGroup.MISC));
 	public static final Item CAPYBARA_MEAT = new Item(new Item.Settings().group(ItemGroup.MISC));
 	public static final Item CAPYBARA_PELT = new Item(new Item.Settings().group(ItemGroup.MISC));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		Registry.register(Registry.ITEM, new Identifier("capybara", "capybara-meat"), CAPYBARA_MEAT);
-		Registry.register(Registry.ITEM, new Identifier("capybara", "capybara-pelt"), CAPYBARA_PELT);
-		Registry.register(Registry.ITEM, new Identifier("capybara", "oak_bark"), OAK_BARK);
+		Registry.register(Registry.ITEM, new Identifier(CAPYBARA_NAMESPACE, "capybara-meat"), CAPYBARA_MEAT);
+		Registry.register(Registry.ITEM, new Identifier(CAPYBARA_NAMESPACE, "capybara-pelt"), CAPYBARA_PELT);
+		Registry.register(Registry.ITEM, new Identifier(CAPYBARA_NAMESPACE, "oak_bark"), OAK_BARK);
 		System.out.println("Hello Fabric world!");
 	}
 }
