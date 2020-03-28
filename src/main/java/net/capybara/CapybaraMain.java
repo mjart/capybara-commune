4a264407a4202f62c15a37c674c0dfd9177aa321
mjart martmenting@gmail.com <martmenting@gmail.com>
2020-03-28 13:36:38 +0100

    Added oakbark itemclass and creative item group: capybara

--- src/main/java/net/capybara/CapybaraMain.java
+++ src/main/java/net/capybara/CapybaraMain.java
@@ -1,17 +1,30 @@
 package net.capybara;
 
+import net.capybara.items.OakBark;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 
 public class CapybaraMain implements ModInitializer {
 
-	public static final Item OAK_BARK = new Item(new Item.Settings().group(ItemGroup.MISC));
-	public static final Item CAPYBARA_MEAT = new Item(new Item.Settings().group(ItemGroup.MISC));
-	public static final Item CAPYBARA_PELT = new Item(new Item.Settings().group(ItemGroup.MISC));
+	// Setup the creative item group
+	public static ItemGroup CAPYBARA = FabricItemGroupBuilder.create(
+			new Identifier("capybara", "general"))
+			.icon(() -> new ItemStack(Items.BOWL))
+			.build();
+
+
+
+	public static final Item OAK_BARK = new OakBark(new Item.Settings().group(CapybaraMain.CAPYBARA));
+	public static final Item CAPYBARA_MEAT = new Item(new Item.Settings().group(CapybaraMain.CAPYBARA));
+	public static final Item CAPYBARA_PELT = new Item(new Item.Settings().group(CapybaraMain.CAPYBARA));
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
---
+++ src/main/java/net/capybara/items/OakBark.java
@@ -0,0 +1,11 @@
+package net.capybara.items;
+
+import net.minecraft.item.Item;
+
+public class OakBark extends Item {
+    public OakBark(Settings settings)
+    {
+        super(settings);
+    }
+
+}
