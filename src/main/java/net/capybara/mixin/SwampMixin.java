0989ae17c1c30adc55d477960b56fc90ad2616f2
FireFlyForLife <maiko.s.99@gmail.com>
2020-04-05 12:32:08 +0200

    Capybara's spawn in the swamp now!

---
+++ src/main/java/net/capybara/mixin/SwampMixin.java
@@ -0,0 +1,26 @@
+package net.capybara.mixin;
+
+import net.capybara.CapybaraMain;
+import net.capybara.entities.passive.CapybaraEntity;
+import net.minecraft.entity.EntityCategory;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.SwampBiome;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SwampBiome.class)
+public abstract class SwampMixin extends Biome {
+    protected SwampMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Inject(at = @At("RETURN"), method = "<init>()V")
+    private void init(CallbackInfo info) {
+        // EntityType<?> type, int weight, int minGroupSize, int maxGroupSize
+        this.addSpawn(EntityCategory.CREATURE, new Biome.SpawnEntry(CapybaraMain.CAPYBARA_MOB, 150, 5, 15 ));
+
+        System.out.println("Mixed in spawning of capybara's in the swamp!");
+    }
+}
